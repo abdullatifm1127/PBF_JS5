23d3f2fd4c931c512e308e2e50b5a133943e7608
latif <mabdullatifm1127@gmail.com>
2026-03-04 08:27:51 +0700

    LAngkah 5

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,23 +1,10 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "./login.module.css";
+import TampilanLogin from "../views/auth/login";
 
 const halamanLogin = () => {
-    const router = useRouter();
-    const handleLogin = (destination: string) => {
-        console.log(`Button clicked, navigating to ${destination}`); 
-        localStorage.setItem("isLogin", "true");
-
-        document.cookie = "isLogin=true; path=/"; // Set cookie untuk status login
-        router.push(destination);
-    }
     return (
-        <div className={styles.login}>
-            <h1>Halaman Login</h1>
-            <button onClick={() => handleLogin("/produk")}>Login ke Produk (Imperatif)</button> <br />
-            <button onClick={() => handleLogin("/shop")}>Login ke Shop</button>
-            <Link href="/auth/register">Ke Halaman Register (Link)</Link>
-        </div>
+        <>
+        <TampilanLogin/>
+        </>
     );
 };
 
